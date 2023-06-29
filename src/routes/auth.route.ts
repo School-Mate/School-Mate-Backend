a4fb3c3af41819449d26187c323b6ff7dc92d18c
YongChan Cho <h3236516@gmail.com>
2023-06-29 23:39:14 +0900

    feat(routes): add google, kakao

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
 import AuthController from '@controllers/auth.controller';
-import { CreateUserDto } from '@dtos/users.dto';
 import { Routes } from '@interfaces/routes.interface';
 import authMiddleware from '@middlewares/auth.middleware';
-import validationMiddleware from '@middlewares/validation.middleware';
 
 class AuthRoute implements Routes {
   public path = '/auth';
@@ -16,8 +14,9 @@ class AuthRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}/kakao`, this.authController.kakaoLogin);
-    // this.router.post(`${this.path}signup`, validationMiddleware(CreateUserDto, 'body'), this.authController.signUp);
-    this.router.post(`${this.path}login`, validationMiddleware(CreateUserDto, 'body'), this.authController.logIn);
+    this.router.get(`${this.path}/kakao/callback`, this.authController.kakaoLoginCallback);
+    this.router.get(`${this.path}/google`, this.authController.googleLogin);
+    this.router.get(`${this.path}/google/callback`, this.authController.googleLoginCallback);
     this.router.post(`${this.path}logout`, authMiddleware, this.authController.logOut);
   }
 }
