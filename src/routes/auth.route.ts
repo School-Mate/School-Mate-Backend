96e480487f1f8d19485c45ce8d7d59d7a54d6187
Kuah_0 <gdsygrt557422@naver.com>
2023-06-29 23:09:56 +0900

    feat(login): Google OAuth

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,7 +6,7 @@ import authMiddleware from '@middlewares/auth.middleware';
 import validationMiddleware from '@middlewares/validation.middleware';
 
 class AuthRoute implements Routes {
-  public path = '/';
+  public path = '/auth';
   public router = Router();
   public authController = new AuthController();
 
@@ -15,8 +15,10 @@ class AuthRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.post(`${this.path}signup`, validationMiddleware(CreateUserDto, 'body'), this.authController.signUp);
-    this.router.post(`${this.path}login`, validationMiddleware(CreateUserDto, 'body'), this.authController.logIn);
+    this.router.get(`${this.path}/google`, this.authController.googleLogin);
+    this.router.get(`${this.path}/google/callback`, this.authController.googleLoginCallback);
+    // this.router.post(`${this.path}signup`, validationMiddleware(CreateUserDto, 'body'), this.authController.signUp);
+    // this.router.post(`${this.path}login`, validationMiddleware(CreateUserDto, 'body'), this.authController.logIn);
     this.router.post(`${this.path}logout`, authMiddleware, this.authController.logOut);
   }
 }
