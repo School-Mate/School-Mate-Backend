897662431b10f2d19573c0e222f2ff565999e2cf
YongChan Cho <h3236516@gmail.com>
2023-06-29 19:01:17 +0900

    chore(env): add kakao environments

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,5 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, DOMAIN } = process.env;
+export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, DOMAIN, KAKAO_CLIENT_KEY, KAKAO_CLIENT_SECRET, KAKAO_REDIRECT_URI } =
+  process.env;
