52cce3e95f277925d6772d6d833f65c511adda9d
Dohyun Kim <kim@dohyun.kr>
2023-07-02 00:00:02 +0900

    style(auth): Remove Not Useing Annotation, Import

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,11 +1,10 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { User } from '@prisma/client';
 import { RequestWithUser } from '@interfaces/auth.interface';
 import AuthService from '@services/auth.service';
 import { GOOGLE_REDIRECT_URI, GOOGLE_CLIENT_KEY, KAKAO_CLIENT_KEY, KAKAO_REDIRECT_URI } from '@/config';
 import ResponseWrapper from '@/utils/responseWarpper';
 import { RequestHandler } from '@/interfaces/routes.interface';
-import { HttpException } from '@/exceptions/HttpException';
 import { CreateUserDto, VerifyPhoneCodeDto, VerifyPhoneMessageDto } from '@/dtos/users.dto';
 
 class AuthController {
@@ -78,17 +77,6 @@ class AuthController {
     }
   };
 
-  // public signUp = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  //   try {
-  //     const userData: CreateUserDto = req.body;
-  //     const signUpUserData: User = await this.authService.signup(userData);
-
-  //     res.status(201).json({ data: signUpUserData, message: 'signup' });
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // };
-
   public kakaoLoginCallback = async (req: RequestHandler, res: Response, next: NextFunction): Promise<void> => {
     try {
       const { code } = req.query;
@@ -114,29 +102,6 @@ class AuthController {
       next(error);
     }
   };
-
-  // public signUp = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  //   try {
-  //     const userData: CreateUserDto = req.body;
-  //     const signUpUserData: User = await this.authService.signup(userData);
-
-  //     res.status(201).json({ data: signUpUserData, message: 'signup' });
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // };
-
-  // public logIn = async (req: RequestHandler, res: Response, next: NextFunction): Promise<void> => {
-  //   try {
-  //     const userData: CreateUserDto = req.body;
-  //     const { cookie, findUser } = await this.authService.login(userData);
-
-  //     res.setHeader('Set-Cookie', [cookie]);
-  //     res.status(200).json({ data: findUser, message: 'login' });
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // };
 
   public logOut = async (req: RequestWithUser, res: Response, next: NextFunction): Promise<void> => {
     try {
