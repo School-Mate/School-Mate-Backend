6b1b0fe361574ac92ba26bc0174e7fa55d256863
YongChan Cho <h3236516@gmail.com>
2023-07-06 11:52:12 +0900

    feat(dtos): add school dto

--- src/dtos/school.dto.ts
+++ src/dtos/school.dto.ts
@@ -1,4 +1,4 @@
-import { IsDateString, IsOptional, IsString, ValidateIf } from 'class-validator';
+import { IsDateString, IsNumber, IsOptional, IsString, ValidateIf } from 'class-validator';
 
 export class SearchSchoolDto {
   @IsString({ message: '검색할 학교 이름을 입력해주세요.' })
@@ -17,4 +17,32 @@ export class GetMealDto {
   @ValidateIf(o => !o.startDate && !o.endDate)
   @IsDateString({ strict: true }, { message: '날짜 형식이 아닙니다.' })
   public date: string;
+}
+
+export class GetTimetableDto {
+  @IsNumber()
+  public grade: number;
+
+  @IsNumber()
+  public class: number;
+
+  @IsOptional()
+  @IsNumber()
+  public semes: number;
+
+  @IsOptional()
+  @IsString()
+  public dept: string;
+
+  @IsOptional()
+  @IsNumber()
+  public year: number;
+
+  @ValidateIf(o => o.endDate)
+  @IsDateString({ strict: true }, { message: '날짜 형식이 아닙니다.' })
+  public startDate: string;
+
+  @ValidateIf(o => o.startDate)
+  @IsDateString({ strict: true }, { message: '날짜 형식이 아닙니다.' })
+  public endDate: string;
 }
