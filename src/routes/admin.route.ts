0261e5922f85a8ece30998435c32051c8eb683e6
YongChan Cho <h3236516@gmail.com>
2023-07-07 14:58:12 +0900

    feat(routes): add admin router

---
+++ src/routes/admin.route.ts
@@ -0,0 +1,24 @@
+import AdminController from '@/controllers/admin.controller';
+import { AdminDto } from '@/dtos/admin.dto';
+import { Routes } from '@/interfaces/routes.interface';
+import adminMiddleware from '@/middlewares/admin.middleware';
+import validationMiddleware from '@/middlewares/validation.middleware';
+import { Router } from 'express';
+
+class AdminRoute implements Routes {
+  public path = '/admin';
+  public router = Router();
+  public adminController = new AdminController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}/auth/me`, adminMiddleware, this.adminController.me);
+    this.router.post(`${this.path}/auth/signup`, validationMiddleware(AdminDto, 'body'), this.adminController.signUp);
+    this.router.post(`${this.path}/auth/login`, validationMiddleware(AdminDto, 'body'), this.adminController.login);
+  }
+}
+
+export default AdminRoute;
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,10 @@ import IndexRoute from '@routes/index.route';
 import validateEnv from '@utils/validateEnv';
 import SchoolRoute from './routes/school.route';
 import BusRoute from './routes/bus.route';
+import AdminRoute from './routes/admin.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new AuthRoute(), new SchoolRoute(), new BusRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute(), new SchoolRoute(), new BusRoute(), new AdminRoute()]);
 
 app.listen();
