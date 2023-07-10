f6100f7631d2582add0a968b6c7f5a81a747aea8
YongChan Cho <h3236516@gmail.com>
2023-07-10 12:22:45 +0900

    feat(route): create board router

---
+++ src/routes/board.route.ts
@@ -0,0 +1,19 @@
+import BoardController from '@/controllers/board.controller';
+import { Routes } from '@/interfaces/routes.interface';
+import { Router } from 'express';
+
+class BoardRoute implements Routes {
+  public path = '/board';
+  public router = Router();
+  public boardController = new BoardController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}/:boardId`, this.boardController.getBoardInfo);
+  }
+}
+
+export default BoardRoute;
--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,10 @@ import validateEnv from '@utils/validateEnv';
 import SchoolRoute from './routes/school.route';
 import BusRoute from './routes/bus.route';
 import AdminRoute from './routes/admin.route';
+import BoardRoute from './routes/board.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new AuthRoute(), new SchoolRoute(), new BusRoute(), new AdminRoute()]);
+const app = new App([new IndexRoute(), new AuthRoute(), new SchoolRoute(), new BusRoute(), new AdminRoute(), new BoardRoute()]);
 
 app.listen();
