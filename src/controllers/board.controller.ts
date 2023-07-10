a0aaa55e52f497ff775b1463a5c17ff28999999c
YongChan Cho <h3236516@gmail.com>
2023-07-10 12:22:35 +0900

    feat(controller): create board controller

---
+++ src/controllers/board.controller.ts
@@ -0,0 +1,23 @@
+import BoardService from '@/services/board.service';
+import ResponseWrapper from '@/utils/responseWarpper';
+import { Response, NextFunction } from 'express';
+import { RequestHandler } from '@/interfaces/routes.interface';
+
+class BoardController {
+  public boardService = new BoardService();
+
+  public getBoardInfo = async (req: RequestHandler, res: Response, next: NextFunction) => {
+    try {
+      const boardId = req.params.boardId;
+      const boardData = await this.boardService.getBoardInfo(boardId);
+
+      ResponseWrapper(req, res, {
+        data: boardData,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default BoardController;
