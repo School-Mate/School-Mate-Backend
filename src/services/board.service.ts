5094a4ad290a13fee6caaa64f14cdf07494c9166
Dohyun Kim <kim@dohyun.kr>
2023-07-11 16:48:16 +0900

    fix: board default data

--- src/services/board.service.ts
+++ src/services/board.service.ts
@@ -17,6 +17,61 @@ class BoardService {
         },
       });
 
+      if (findBoards.length === 0) {
+        await this.board.createMany({
+          data: [
+            {
+              name: '자유게시판',
+              description: '자유롭게 글을 쓸 수 있는 게시판입니다.',
+              schoolId: user.userSchoolId,
+            },
+            {
+              name: '취미공유',
+              description: '취미를 공유하는 게시판입니다.',
+              schoolId: user.userSchoolId,
+            },
+            {
+              name: '질문게시판',
+              description: '질문을 할 수 있는 게시판입니다.',
+              schoolId: user.userSchoolId,
+            },
+            {
+              name: '새내기게시판',
+              description: '새내기들을 위한 게시판입니다.',
+              schoolId: user.userSchoolId,
+            },
+            {
+              name: '연애고민해결',
+              description: '연애고민을 해결해주는 게시판입니다.',
+              schoolId: user.userSchoolId,
+            },
+            {
+              name: '스터디게시판',
+              description: '공부시간, 플레너 인증, 스터디정보를 공유하는 게시판입니다.',
+              schoolId: user.userSchoolId,
+            },
+            {
+              name: '졸업생게시판',
+              description: '졸업생들을 위한 게시판입니다.',
+              schoolId: user.userSchoolId,
+            },
+            {
+              name: '내찍사게시판',
+              description: '내가찍은사진을 올리는 게시판입니다.',
+              schoolId: user.userSchoolId,
+            },
+          ],
+        });
+
+        const findBoards = await this.board.findMany({
+          where: {
+            schoolId: user.userSchoolId,
+          },
+        });
+
+        return findBoards;
+      }
+
       return findBoards;
     } catch (error) {
       throw new HttpException(500, '알 수 없는 오류가 발생했습니다.');
@@ -27,7 +82,7 @@ class BoardService {
     try {
       const findBoard = await this.board.findUnique({
         where: {
-          id: boardId as any as number,
+          id: Number(boardId),
         },
       });
 
@@ -49,7 +104,7 @@ class BoardService {
     try {
       const findBoard = await this.board.findUnique({
         where: {
-          id: Number(boardId) as number,
+          id: Number(boardId),
         },
       });
       if (!findBoard) throw new HttpException(404, '해당하는 게시판이 없습니다.');
@@ -62,7 +117,7 @@ class BoardService {
           images: data.images,
           isAnonymous: data.isAnonymous,
           userId: data.userId,
-          boardId: Number(boardId) as number,
+          boardId: Number(boardId),
         },
       });
     } catch (error) {
@@ -74,7 +129,7 @@ class BoardService {
     try {
       const findArticle = await this.article.findUnique({
         where: {
-          id: articleId as any as number,
+          id: Number(articleId),
         },
       });
 
