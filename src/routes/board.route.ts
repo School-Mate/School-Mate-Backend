1d640e7360b15868458dfe08d7de4feec7f7f9ff
YongChan Cho <h3236516@gmail.com>
2023-11-28 00:57:33 +0900

    feat(route): change route

--- src/routes/board.route.ts
+++ src/routes/board.route.ts
@@ -18,7 +18,7 @@ class BoardRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}`, authMiddleware, this.boardController.getBoards);
-    this.router.get(`${this.path}/suggest`, authMiddleware, this.boardController.getSuggestArticles);
+    this.router.get(`${this.path}/hot`, authMiddleware, this.boardController.getHotArticles);
     this.router.get(`${this.path}/search`, authMiddleware, validationMiddleware(SearchCombineDto, 'query'), this.boardController.searchCombine);
     this.router.get(`${this.path}/:boardId`, authMiddleware, this.boardController.getBoard);
     this.router.get(
