de65e3c17ca451ff88449a94dd64fa2a8b95e078
YongChan Cho <h3236516@gmail.com>
2023-12-10 18:56:36 +0900

    style(admin): style

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -16,17 +16,9 @@ class AdminRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}/me`, adminMiddleware, this.adminController.me);
-    this.router.post(
-      `${this.path}/signup`,
-      validationMiddleware(AdminDto, 'body'),
-      adminMiddleware,
-      adminFlagMiddleware('SUPER_ADMIN'),
-      this.adminController.signUp,
-    );
-    this.router.post(`${this.path}/login`, validationMiddleware(AdminDto, 'body'), this.adminController.login);
     this.router.get(`${this.path}/logout`, adminMiddleware, this.adminController.logOut);
-    this.router.delete(`${this.path}/image/:id`, adminMiddleware, adminFlagMiddleware('USER_MANAGE'), this.adminController.deleteImage);
     this.router.get(`${this.path}/user/all`, adminMiddleware,adminFlagMiddleware('USER_MANAGE'), this.adminController.getAllUsers);
+    this.router.get(`${this.path}/user/:userId`, adminMiddleware, adminFlagMiddleware('USER_MANAGE'), this.adminController.getUserInfo);
     this.router.get(
       `${this.path}/verify`,
       adminMiddleware,
@@ -34,12 +26,12 @@ class AdminRoute implements Routes {
       adminFlagMiddleware('USER_SCHOOL_REVIEWER'),
       this.adminController.verifyRequests,
     );
-    this.router.post(
-      `${this.path}/verify`,
+    this.router.get(
+      `${this.path}/report`,
       adminMiddleware,
-      validationMiddleware(AdminRequestDto, 'body'),
-      adminFlagMiddleware('USER_SCHOOL_REVIEWER'),
-      this.adminController.postVerifyRequest,
+      validationMiddleware(GetReportRequestDto, 'query'),
+      adminFlagMiddleware('USER_REPORT_REVIEWER'),
+      this.adminController.reports,
     );
     this.router.get(
       `${this.path}/board`,
@@ -49,18 +41,26 @@ class AdminRoute implements Routes {
       this.adminController.boardRequests,
     );
     this.router.post(
+      `${this.path}/signup`,
+      validationMiddleware(AdminDto, 'body'),
+      adminMiddleware,
+      adminFlagMiddleware('SUPER_ADMIN'),
+      this.adminController.signUp,
+    );
+    this.router.post(`${this.path}/login`, validationMiddleware(AdminDto, 'body'), this.adminController.login);
+    this.router.post(
+      `${this.path}/verify`,
+      adminMiddleware,
+      validationMiddleware(AdminRequestDto, 'body'),
+      adminFlagMiddleware('USER_SCHOOL_REVIEWER'),
+      this.adminController.postVerifyRequest,
+    );
+    this.router.post(
       `${this.path}/board`,
       adminMiddleware,
       validationMiddleware(AdminRequestDto, 'body'),
       adminFlagMiddleware('BOARD_MANAGE'),
       this.adminController.postBoardRequest,
-    );
-    this.router.get(
-      `${this.path}/report`,
-      adminMiddleware,
-      validationMiddleware(GetReportRequestDto, 'query'),
-      adminFlagMiddleware('USER_REPORT_REVIEWER'),
-      this.adminController.reports,
     );
     this.router.post(
       `${this.path}/report`,
@@ -75,7 +75,7 @@ class AdminRoute implements Routes {
       adminFlagMiddleware('USER_MANAGE'),
       this.adminController.deleteBoardArticle,
     );
-    this.router.get(`${this.path}/user/:userId`, adminMiddleware, adminFlagMiddleware('USER_MANAGE'), this.adminController.getUserInfo);
+    this.router.delete(`${this.path}/image/:id`, adminMiddleware, adminFlagMiddleware('USER_MANAGE'), this.adminController.deleteImage);
   }
 }
 
