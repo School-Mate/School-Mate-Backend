7299f317122ea6710f27eddefcc8961cd9265bb7
Dohyun Kim <kim@dohyun.kr>
2023-12-12 12:10:24 +0900

    feat: date check

--- src/dtos/ad.dto.ts
+++ src/dtos/ad.dto.ts
@@ -1,4 +1,4 @@
-import { IsDate, IsOptional, IsString } from "class-validator";
+import { IsDate, IsDateString, IsOptional, IsString } from 'class-validator';
 
 export class AdDto {
   @IsString({ message: '광고 제목을 입력해주세요.' })
@@ -10,10 +10,10 @@ export class AdDto {
   @IsString({ message: '광고 링크를 추가해주세요.' })
   public link: string;
 
-  @IsDate({ message: '광고 시작 날짜를 추가해주세요.' })
+  @IsDateString({ message: '광고 시작 날짜를 추가해주세요.' })
   @IsOptional()
   public startDate: Date;
 
-  @IsDate({ message: '광고 종료 날짜를 추가해주세요.' })
+  @IsDateString({ message: '광고 종료 날짜를 추가해주세요.' })
   public endDate: Date;
 }
