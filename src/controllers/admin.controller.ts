56b5bb330dbd9a79ace3f2ddb19d58175162a0d7
YongChan Cho <h3236516@gmail.com>
2023-12-26 14:15:25 +0900

    style(admin): remove unused import

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,4 +1,4 @@
-import { AdminDto, GetVerifyRequestDto, AdminRequestDto, UserBlockDto } from '@/dtos/admin.dto';
+import { AdminDto, AdminRequestDto, UserBlockDto } from '@/dtos/admin.dto';
 import { AdminService } from '@/services/admin.service';
 import ResponseWrapper from '@/utils/responseWarpper';
 import { Admin, ReportTargetType } from '@prisma/client';
