89b3c5f7ee21fc12d91a82e0bcd626de194ad4ed
kiss8981 <kdh89811@gmail.com>
2023-12-30 15:20:07 +0800

    fix(sms): rate limiter

--- src/middlewares/ratelimit.middleware.ts
+++ src/middlewares/ratelimit.middleware.ts
@@ -18,7 +18,7 @@ export const verifyPhoneRateLimit = rateLimit({
   windowMs: 60 * 1000 * 30,
   max: 3,
   statusCode: 429,
-  keyGenerator: req => (req.headers['x-forwarded-for'] ? (req.headers['x-forwarded-for'] as string) : req.ip),
+  keyGenerator: req => req.body.phone,
   handler(req, res) {
     ResponseWrapper(req as any, res, {
       message: '너무 많은 요청을 보냈습니다. 잠시 후 다시 시도해주세요.',
